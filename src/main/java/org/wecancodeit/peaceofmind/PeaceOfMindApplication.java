b27b0992be00c34fb4d437931751536b6f82fe07
Izzy Elwyn <izzy.elwyn@gmail.com>
2018-11-03 14:04:45 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/peaceofmind/PeaceOfMindApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.peaceofmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaceOfMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaceOfMindApplication.class, args);
+	}
+}

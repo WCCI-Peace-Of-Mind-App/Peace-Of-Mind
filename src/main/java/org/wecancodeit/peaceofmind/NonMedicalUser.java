451eecfbc2e34b8325e2497c3d59cdeaed9b78e9
Todd <mopeybear@gmail.com>
2018-11-03 15:12:04 -0400

    All fields created for NonMedicalUser class

--- src/main/java/org/wecancodeit/peaceofmind/NonMedicalUser.java
+++ src/main/java/org/wecancodeit/peaceofmind/NonMedicalUser.java
@@ -2,12 +2,52 @@ package org.wecancodeit.peaceofmind;
 
 public class NonMedicalUser {
 
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String contactNumber;
+	private String workNumber;
+	private String username;
+	private String password;
+
 	public String getFirstName() {
-		return "Joe";
+		return firstName;
 	}
 
 	public String getLastName() {
-		return "Bob";
+		return lastName;
 	}
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public String getWorkNumber() {
+		return workNumber;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public NonMedicalUser(String firstName, String lastName, String address, String contactNumber, String workNumber, String username, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.contactNumber = contactNumber;
+		this.workNumber = workNumber;
+		this.username = username;
+		this.password = password;
+	}
+
+
 
 }
--- src/test/java/org/wecancodeit/peaceofmind/NonMedicalUserTest.java
+++ src/test/java/org/wecancodeit/peaceofmind/NonMedicalUserTest.java
@@ -11,21 +11,79 @@ public class NonMedicalUserTest {
 	NonMedicalUser underTest;
 	String firstName;
 	String lastName;
+	String address;
+	String contactNumber;
+	String workNumber;
+	String username;
+	String password;
 	
 	@Before
 	public void setUp() {
-		
+		firstName = "Joe";
+		lastName = "Bob";
+		address = "1123 Sesame Street, NoWhere, OH 43101";
+		contactNumber = "614-837-5309";
+		workNumber = "555-555-5555";
+		username = "Mopamus Wrex";
+		password = "Password123";
+
+		underTest = new NonMedicalUser(firstName, lastName, address, contactNumber, workNumber, username, password);
 	}
 	
 	@Test
-	public void shouldHaveAFirstAndLastName() {
-		firstName = "Joe";
-		lastName = "Bob";
-		underTest = new NonMedicalUser();
+	public void shouldHaveAFirstAndLastNameOfJoe_Bob() {
 		String testFirstName = underTest.getFirstName();
 		String testLastName = underTest.getLastName();
 		assertThat(testFirstName, is(firstName));
 		assertThat(testLastName, is(lastName));
 	}
+	
+	
+	@Test
+	public void testAddressIs1123SesameStreetNoWhereOH43101() {
+		String testAddress = underTest.getAddress();
+		assertThat(testAddress, is(address));
+	}
+	
+	@Test
+	public void shouldHaveContactNumberOf6148375309() {
+		String testContactNumber = underTest.getContactNumber();
+		assertThat(testContactNumber, is(contactNumber));
+	}
+	
+	@Test
+	public void shouldHaveWorkNumberOf5555555555() {
+		String testWorkNumber = underTest.getWorkNumber();
+		assertThat(testWorkNumber, is(workNumber));
+	}
+	
+	@Test
+	public void shouldHaveUserNameOfMopamusWrex() {
+		String testUsername = underTest.getUsername();
+		assertThat(testUsername, is(username));
+	}
+	
+	@Test
+	public void shouldHavePasswordOfPassword123() {
+		String testPassword = underTest.getPassword();
+		assertThat(testPassword, is(password));
+	}
 
+	@Test
+	public void test2ndUserWithAllDifferentValues() {
+		firstName = "Todd";
+		lastName = "Kenzie";
+		address = "789 Market Square, Dublin, OH 41010";
+		contactNumber = "123-456-7890";
+		workNumber = "987-654-3210";
+		username = "xxxGAMERxxx";
+		password = "123Eureka!";
+		
+		underTest = new NonMedicalUser(firstName, lastName, address, contactNumber, workNumber, username, password);
+		String testFirstName = underTest.getFirstName();
+		String testLastName = underTest.getLastName();
+		assertThat(testFirstName, is(firstName));
+		assertThat(testLastName, is(lastName));
+	}
+	
 }

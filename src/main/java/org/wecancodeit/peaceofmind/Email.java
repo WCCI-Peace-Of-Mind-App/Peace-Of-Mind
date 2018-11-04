5ae55f81b512cee9a5e920eefc3e6deefea0886e
Izzy Elwyn <izzy.elwyn@gmail.com>
2018-11-04 16:24:01 -0500

    adding foreign key and hashcodes

--- src/main/java/org/wecancodeit/peaceofmind/Email.java
+++ src/main/java/org/wecancodeit/peaceofmind/Email.java
@@ -3,6 +3,7 @@ package org.wecancodeit.peaceofmind;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -13,6 +14,7 @@ public class Email implements IContactType {
 	private long id;
 	
 	@ManyToOne
+	@JoinColumn(name="fk_contactInfo")
 	private ContactInfo contactInfo;
 	
 	private String emailAddress;
@@ -41,6 +43,30 @@ public class Email implements IContactType {
 	public Email(String emailAddress, String type) {
 		this.emailAddress = emailAddress;
 		this.type = type;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Email other = (Email) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 

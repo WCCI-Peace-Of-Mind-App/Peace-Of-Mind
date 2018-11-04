82089e3feefbb7ecb9d2ce0301bab50ef945cdeb
MachariaKuria <skuria@student.cscc.edu>
2018-11-04 16:48:01 -0500

    updated

--- src/main/java/org/wecancodeit/peaceofmind/IContactInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.wecancodeit.peaceofmind;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IContactInfoRepository extends CrudRepository<ContactInfo, Long> {
-
-}

e212d4fcec72dd657ff93e1c078304b05610c1ce
Todd <mopeybear@gmail.com>
2018-11-04 15:49:58 -0500

    Phone Repo works and tested.  2 lines need to be added to ContactInfo once testing is finalized (not entered here)

--- src/main/java/org/wecancodeit/peaceofmind/PhoneRepository.java
+++ src/main/java/org/wecancodeit/peaceofmind/PhoneRepository.java
@@ -1,7 +1,11 @@
 package org.wecancodeit.peaceofmind;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PhoneRepository extends CrudRepository<Phone, Long> {
+
+	Collection<Phone> findByContactInfo(ContactInfo contactInfo);
 
 }

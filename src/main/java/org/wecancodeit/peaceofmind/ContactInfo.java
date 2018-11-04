b92991f9eda071e45325ed45286e2e47c238f7e4
Izzy Elwyn <izzy.elwyn@gmail.com>
2018-11-04 15:32:18 -0500

    Revert "contactInfo additions"
    This reverts commit 011ec7b433a2592c95a1caeea61ca249e3a63e5a.

--- src/main/java/org/wecancodeit/peaceofmind/ContactInfo.java
+++ src/main/java/org/wecancodeit/peaceofmind/ContactInfo.java
@@ -21,13 +21,10 @@ public class ContactInfo {
 	@OneToMany
 	private Collection<Phone> phones;
 	
-	//private Collection<String> emails;
+//	private Collection<String> emails;
 
 	@OneToOne(mappedBy = "contactInfo")
 	private Patient patient;
-	
-	@OneToOne(mappedBy = "contactInfo")
-	private NonMedicalUser nonMedicalUser;
 	
 	public long getId() {
 		return id;
@@ -41,15 +38,11 @@ public class ContactInfo {
 		return phones;
 	}
 	
-	//public Collection<String> getEmails() {
-	//	return emails;
-	//}
+//	public Collection<String> getEmails() {
+//		return emails;
+//	}
 	public Patient getPatient() {
 		return patient;
-	}
-	
-	public NonMedicalUser getNonMedicalUser() {
-		return nonMedicalUser;
 	}
 
 	public ContactInfo() {}
@@ -57,8 +50,8 @@ public class ContactInfo {
 	public ContactInfo(Collection<Address> addresses, Collection<Phone>phones, Collection<String> emails) {
 		this.addresses = addresses;
 		this.phones = phones;
-//		this.emails = emails;	
-		}
+//		this.emails = emails;
+	}
 
 	@Override
 	public int hashCode() {

6f123c546d9d62ed12ff03cd99b27d4ee486ee1b
Michael-Mosher <michael.r.mosher@gmail.com>
2018-11-27 19:17:14 -0500

    added necessary annotations to PomWebSecurityAdapter

--- src/main/java/org/wecancodeit/peaceofmind/PomWebSecurityAdapter.java
+++ src/main/java/org/wecancodeit/peaceofmind/PomWebSecurityAdapter.java
@@ -1,13 +1,19 @@
 package org.wecancodeit.peaceofmind;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true) // Enables @PreAuthorize, etc.
 public class PomWebSecurityAdapter extends WebSecurityConfigurerAdapter {
   @Bean
   public UserDetailsService userDetailsService()
@@ -30,9 +36,9 @@ public class PomWebSecurityAdapter extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception
   {
     http.authorizeRequests()
-        .antMatchers("/h2-console", "/css/**", "/js/**").permitAll()
+        .antMatchers("/h2-console/**", "/css/**", "/js/**").permitAll()
         .anyRequest().authenticated()
-        //.and().csrf().ignoringAntMatchers("/h2-console/**")
+        .and().csrf().ignoringAntMatchers("/h2-console/**")
         .and()
     .formLogin()
         .loginPage("/login").permitAll()

24882a9c183bf94662e7b7523fa2e535033926e7
Macharia Kuria <skuria@student.cscc.edu>
2018-11-29 18:09:11 -0500

    Added collapsibles on the non-med-user template on medication

---
+++ src/main/resources/static/js/javascript.js
@@ -0,0 +1,9 @@
+function expandText(div) {
+  var className = div.getAttribute("class");
+  if(className=="collapsed-par") {
+    div.className = "expanded-par";
+  }
+  else{
+    div.className = "collapsed-par";
+  }
+}
